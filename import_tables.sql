36bec5ba4783de313df96c3275c7f6fce960e656
Monika DesLauriers <154292011+Mdeslauriers2@users.noreply.github.com>
2024-03-14 20:43:29 -0500

    Add files via upload

---
+++ import_tables.sql
@@ -0,0 +1,11 @@
+--import category.csv
+SELECT * FROM category
+
+--import subcategory.csv
+SELECT * FROM subcategory
+
+--import contacts.csv
+SELECT * FROM contacts
+
+--import campaign.csv
+SELECT * FROM campaign
